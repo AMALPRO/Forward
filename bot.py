253fcb6b7bfbe8806891532e7319bd0dd5f305a4
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2023-05-02 01:42:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ mongo_collection = mongo_client[DATABASE_NAME][COLLECTION_NAME]
 ADMIN_USER_ID = ADMINS
 
 # Create a Pyrogram client instance
-app = Client("my_bot", bot_token=6216947279:AAEYIUjQMaL5XMB7MDz2C7kZM5PnZm0Hfc4, api_id=24081768, api_hash="15ead37b9f2c7cf765059a6406903261")
+app = Client("my_bot", bot_token="6216947279:AAEYIUjQMaL5XMB7MDz2C7kZM5PnZm0Hfc4", api_id=24081768, api_hash="15ead37b9f2c7cf765059a6406903261")
 
 # Define a command handler for the /forward_all command
 @app.on_message(filters.command("forward_all") & filters.user(ADMIN_USER_ID))
